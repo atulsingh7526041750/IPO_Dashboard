bd8ddcacdece74983f6dc71981049ad23268a7a4
Atul Singh <atulsingh74088@gmail.com>
2024-01-08 22:31:50 +0530

    This is the code with authenticationand toggling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-// import Dashboard from './components/Dashboard';
-import LoginForm from './Authentication/LoginForm';
+import Dashboard from './components/Dashboard';
+// import LoginForm from './Authentication/LoginForm';
 // import RegistrationForm from './Authentication/RegistrationForm';
 
 function App() {
   return (
     <div className="App">
-      {/* <Dashboard/> */}
+      {/*  */}
       {/* 
        */}
       {/*  */}
       {/* <RegistrationForm /> */}
-      <LoginForm />
+      {/* <LoginForm /> */}
+
+      <Dashboard/>
     </div>
   );
 }
